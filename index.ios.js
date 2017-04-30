4148b9963066f9e103c2f3ddce797917fc82140b
gong023 <gon.gong.gone@gmail.com>
2017-04-30 03:22:10 -0700

    separate bootstrap configure react-redux

--- index.ios.js
+++ index.ios.js
@@ -4,54 +4,7 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import Config from 'react-native-config';
-
-export default class AnnictClient extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-        <Text style={styles.instructions}>
-          {Config.CLIENT_SECRET}
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+import { AppRegistry } from 'react-native';
+import AnnictClient from './js/bootstrap';
 
 AppRegistry.registerComponent('AnnictClient', () => AnnictClient);
---
+++ js/bootstrap.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import App from './containers/App';
+
+const AnnictClient = () =>
+  <App />;
+
+export default AnnictClient;
