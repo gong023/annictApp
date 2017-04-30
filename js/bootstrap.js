937e535455f1b20508f449f1263aee5dc6ceef7b
gong023 <gon.gong.gone@gmail.com>
2017-04-30 16:15:57 -0700

    configure store

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { createEpicMiddleware } from 'redux-observable';
+import { applyMiddleware, createStore } from 'redux';
 import App from './containers/App';
+import epics from './epics';
+import reducer from './reducers';
+
+const epicMiddleware = createEpicMiddleware(epics);
+
+const configureStore = () => (createStore(reducer, applyMiddleware(epicMiddleware)));
 
 const AnnictClient = () =>
-  <App />;
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>;
 
 export default AnnictClient;
