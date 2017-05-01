590e8b84e17cf724d6e5a4332c9f629da1d9620d
gong023 <gon.gong.gone@gmail.com>
2017-04-30 23:31:05 -0700

    configure redux-logger

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { createEpicMiddleware } from 'redux-observable';
 import { applyMiddleware, createStore } from 'redux';
 import axios from 'axios';
+import logger from 'redux-logger';
 import App from './containers/App';
 import epics from './epics';
 import reducer from './reducers';
@@ -10,7 +11,7 @@ import reducer from './reducers';
 axios.defaults.baseURL = 'https://api.annict.com';
 
 const epicMiddleware = createEpicMiddleware(epics);
-const middleware = [epicMiddleware];
+const middleware = [logger, epicMiddleware];
 const store = () => createStore(reducer, applyMiddleware(...middleware));
 
 const AnnictClient = () =>
